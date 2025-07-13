2524c63bf319d75efd428a8e71e0cbb15608aabf
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-13 19:04:57 +0100

    Create webpack.config.js

---
+++ 0x04-TypeScript/task_3/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true
+  }
+};
